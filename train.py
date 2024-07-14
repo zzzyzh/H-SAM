413ff4ec3344b8c17b9d138c0b99cdc87367afc8
zzzyzh <zzzyzh@qq.com>
2024-07-14 15:11:35 +0800

    Update

--- train.py
+++ train.py
@@ -11,7 +11,6 @@ from importlib import import_module
 from segment_anything import sam_model_registry
 
 from trainer import trainer
-from icecream import ic
 
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '1,2'
@@ -23,15 +22,15 @@ parser.add_argument('--task', type=str,
 parser.add_argument('--dataset', type=str,
                     default='sabs_sammed', help='dataset name')
 parser.add_argument('--root_path', type=str,
-                    default='/home/yanzhonghao/data', help='root dir for data')
-parser.add_argument('--output', type=str, default='/home/yanzhonghao/data/experiments/h_sam')
+                    default='../../data', help='root dir for data')
+parser.add_argument('--output', type=str, default='../../data/experiments/h_sam')
 parser.add_argument('--list_dir', type=str,
                     default='./lists/lists_sabs', help='list dir')
 parser.add_argument('--split', type=str,
                     default='train', help='list dir')
 parser.add_argument('--vit_name', type=str,
                     default='vit_b', help='select one vit model')
-parser.add_argument('--ckpt', type=str, default='/home/yanzhonghao/data/experiments/weights/sam_vit_b_01ec64.pth',
+parser.add_argument('--ckpt', type=str, default='../../data/experiments/weights/sam_vit_b_01ec64.pth',
                     help='Pretrained checkpoint')
 parser.add_argument('--lora_ckpt', type=str, default=None, help='Finetuned lora checkpoint')
 parser.add_argument('--scale', type=int, default=127)
