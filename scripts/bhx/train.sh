715398993e7813d034b3d51dce88af8bab7081d9
zzzyzh <zzzyzh@qq.com>
2024-07-18 09:55:36 +0800

    Update

--- scripts/bhx/train.sh
+++ scripts/bhx/train.sh
@@ -1,8 +1,8 @@
 # 0.1
-python train.py --n_gpu 2 --split=train --scale=882 --task=ven --dataset=bhx_sammed --num_classes=4 --list_dir=./lists/lists_bhx --batch_size=6 --base_lr=0.0025 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=/home/yanzhonghao/data/experiments/weights/sam_vit_b_01ec64.pth
+python train.py --n_gpu 2 --split=train --scale=882 --task=ven --dataset=bhx_sammed --num_classes=4 --list_dir=./lists/lists_bhx --batch_size=6 --base_lr=0.003 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=/home/yanzhonghao/data/experiments/weights/sam_vit_b_01ec64.pth
 
 # 0.05
-python train.py --n_gpu 2 --split=train --scale=441 --task=ven --dataset=bhx_sammed --num_classes=4 --list_dir=./lists/lists_bhx --batch_size=6 --base_lr=0.0025 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=/home/yanzhonghao/data/experiments/weights/sam_vit_b_01ec64.pth
+python train.py --n_gpu 2 --split=train --scale=441 --task=ven --dataset=bhx_sammed --num_classes=4 --list_dir=./lists/lists_bhx --batch_size=6 --base_lr=0.003 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=/home/yanzhonghao/data/experiments/weights/sam_vit_b_01ec64.pth
 
 # 0.01
-python train.py --n_gpu 2 --split=train --scale=88 --task=ven --dataset=bhx_sammed --num_classes=4 --list_dir=./lists/lists_bhx --batch_size=6 --base_lr=0.0025 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=/home/yanzhonghao/data/experiments/weights/sam_vit_b_01ec64.pth
+python train.py --n_gpu 2 --split=train --scale=88 --task=ven --dataset=bhx_sammed --num_classes=4 --list_dir=./lists/lists_bhx --batch_size=6 --base_lr=0.003 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=/home/yanzhonghao/data/experiments/weights/sam_vit_b_01ec64.pth
