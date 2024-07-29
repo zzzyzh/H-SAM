40062de11a663101ae40a60f8a66698f65159d79
zzzyzh <zzzyzh@qq.com>
2024-07-29 14:45:33 +0800

    Update

--- scripts/sabs/train.sh
+++ scripts/sabs/train.sh
@@ -1,8 +1,8 @@
 # 0.1
-python train.py --split=train --scale=127 --batch_size=16 --base_lr=0.005 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=../../data/experiments/weights/sam_vit_b_01ec64.pth
+CUDA_VISIBLE_DEVICES=2 python train.py --split=train --scale=127 --batch_size=16 --base_lr=0.005 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=../../data/experiments/weights/sam_vit_b_01ec64.pth
 
 # 0.05
-python train.py --split=train --scale=63 --batch_size=16 --base_lr=0.005 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=../../data/experiments/weights/sam_vit_b_01ec64.pth
+CUDA_VISIBLE_DEVICES=2 python train.py --split=train --scale=63 --batch_size=16 --base_lr=0.005 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=../../data/experiments/weights/sam_vit_b_01ec64.pth
 
 # 0.01
-python train.py --split=train --scale=12 --batch_size=16 --base_lr=0.005 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=../../data/experiments/weights/sam_vit_b_01ec64.pth
+CUDA_VISIBLE_DEVICES=2 python train.py --split=train --scale=12 --batch_size=16 --base_lr=0.005 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=../../data/experiments/weights/sam_vit_b_01ec64.pth
