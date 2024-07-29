1be3cfdc9d2f2610f8ca8a47dd8dfcc3e7e575f1
zzzyzh <zzzyzh@qq.com>
2024-07-29 12:19:16 +0800

    Update

--- scripts/sabs/train.sh
+++ scripts/sabs/train.sh
@@ -1,4 +1,4 @@
-# # 0.1
+# 0.1
 python train.py --n_gpu 2 --split=train --scale=127 --batch_size=6 --base_lr=0.005 --img_size=512 --warmup --AdamW --max_epochs=300 --vit_name=vit_b --ckpt=../../data/experiments/weights/sam_vit_b_01ec64.pth
 
 # 0.05
